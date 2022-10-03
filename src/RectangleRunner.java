781859a8f85aa93dfdc89dfab0faa8bee168a3a3
rozalenadous <rma9547@nyu.edu>
2022-10-03 09:28:23 -0400

    Initial commit

---
+++ src/RectangleRunner.java
@@ -0,0 +1,22 @@
+public class RectangleRunner {
+    public static void main(String [] args){
+
+Rectangle plot1 = new Rectangle(150,200);
+        Rectangle plot2 = new Rectangle(125);
+        Rectangle plot3 = new Rectangle();
+
+      plot1.setLength(75);
+      plot1.setWidth(75);
+
+        plot2.setLength(75);
+        plot2.setWidth(75);
+
+        plot3.setLength(75);
+        plot3.setWidth(75);
+
+
+        int totalArea = plot1.calculateArea() + plot2.calculateArea() + plot3.calculateArea();
+        System.out.println("These three plots require " + totalArea + " square feet of seed");
+    }
+
+}
